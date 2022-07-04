9e2cd3ac2bd38e42371c29e48eb941b52ef08e83
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2022-07-04 20:10:12 -0300

    Added models

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 import os
-from sqlalchemy import Column, String, create_engine
+
+from flask import jsonify
+from flask_migrate import Migrate
+from sqlalchemy import Column, create_engine, String, DateTime, DECIMAL, Integer, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 import json
+
+from sqlalchemy.orm import relationship
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
@@ -21,27 +26,79 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+    migrate = Migrate(app, db)
 
 
-'''
-Person
-Have title and release year
-'''
+class Product(db.Model):
+    __tablename__ = 'products'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    cost = Column(DECIMAL, nullable=False)
+    size = Column(String)
+    picture_url = Column(String)
 
-class Person(db.Model):
-    __tablename__ = 'People'
-
-    id = Column(db.Integer, primary_key=True)
-    name = Column(String)
-    catchphrase = Column(String)
-
-    def __init__(self, name, catchphrase=""):
+    def __init__(self, name, description, cost, size, picture_url, order_id):
         self.name = name
-        self.catchphrase = catchphrase
+        self.description = description,
+        self.cost = cost
+        self.size = size
+        self.picture_url = picture_url
 
     def format(self):
         return {
             'id': self.id,
             'name': self.name,
-            'catchphrase': self.catchphrase}
+            'description': self.description,
+            'cost': self.cost,
+            'size': self.size,
+            'picture_url': self.picture_url
+        }
+
+
+order_items = db.Table('order_items',
+                       db.Column('order_id', Integer, db.ForeignKey('orders.id'), primary_key=True),
+                       db.Column('product_id', Integer, db.ForeignKey('products.id'), primary_key=True)
+                       )
+
+cart_items = db.Table('cart_items',
+                      db.Column('cart_id', Integer, db.ForeignKey('carts.id'), primary_key=True),
+                      db.Column('product_id', Integer, db.ForeignKey('products.id'), primary_key=True)
+                      )
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    order_number = Column(Integer)
+    order_date = Column(DateTime)
+    status = Column(String)
+    tracking_code = Column(String)
+    products = relationship("products", secondary=order_items,
+                            backref=db.backref('orders', lazy=True))
+
+    def __init__(self, order_number, order_date, status, tracking_code):
+        self.order_number = order_number,
+        self.order_date = order_date,
+        self.status = status,
+        self.tracking_code = tracking_code
+
+    def format(self):
+        return {
+            'id': self.id,
+            'order_number': self.order_number,
+            'order_date': self.order_date,
+            'status': self.status,
+            'tracking_code': self.tracking_code,
+            'products': jsonify(self.products)
+        }
+
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    id = Column(Integer, primary_key=True)
+    products = relationship("Products", secondary=order_items,
+                            backref=db.backref('carts', lazy=True))
