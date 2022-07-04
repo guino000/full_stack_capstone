88228f43a958bae3697bab2f9159de830551c570
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2022-07-04 20:37:18 -0300

    Added models

--- models.py
+++ models.py
@@ -29,34 +29,6 @@ def setup_db(app, database_path=database_path):
     migrate = Migrate(app, db)
 
 
-class Product(db.Model):
-    __tablename__ = 'products'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    cost = Column(DECIMAL, nullable=False)
-    size = Column(String)
-    picture_url = Column(String)
-
-    def __init__(self, name, description, cost, size, picture_url, order_id):
-        self.name = name
-        self.description = description,
-        self.cost = cost
-        self.size = size
-        self.picture_url = picture_url
-
-    def format(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'cost': self.cost,
-            'size': self.size,
-            'picture_url': self.picture_url
-        }
-
-
 order_items = db.Table('order_items',
                        db.Column('order_id', Integer, db.ForeignKey('orders.id'), primary_key=True),
                        db.Column('product_id', Integer, db.ForeignKey('products.id'), primary_key=True)
@@ -66,6 +38,62 @@ cart_items = db.Table('cart_items',
                       db.Column('cart_id', Integer, db.ForeignKey('carts.id'), primary_key=True),
                       db.Column('product_id', Integer, db.ForeignKey('products.id'), primary_key=True)
                       )
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    cart = relationship('carts', back_populates='users', uselist=False)
+    orders = relationship('orders', backref='users', lazy=True)
+
+    def __int__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    cost = Column(DECIMAL, nullable=False)
+    size = Column(String)
+    pictures = relationship('product_pictures', backref='products', lazy=True)
+
+    def __init__(self, name, description, cost, size):
+        self.name = name
+        self.description = description,
+        self.cost = cost
+        self.size = size
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'cost': self.cost,
+            'size': self.size,
+            'pictures': jsonify(self.pictures)
+        }
+
+
+class ProductPictures(db.Model):
+    __tablename__ = 'product_pictures'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    product_id = Column(Integer, ForeignKey('products.id'))
 
 
 class Order(db.Model):
@@ -78,6 +106,7 @@ class Order(db.Model):
     tracking_code = Column(String)
     products = relationship("products", secondary=order_items,
                             backref=db.backref('orders', lazy=True))
+    user_id = Column(Integer, ForeignKey('users.id'))
 
     def __init__(self, order_number, order_date, status, tracking_code):
         self.order_number = order_number,
@@ -102,3 +131,10 @@ class Cart(db.Model):
     id = Column(Integer, primary_key=True)
     products = relationship("Products", secondary=order_items,
                             backref=db.backref('carts', lazy=True))
+    user = relationship('users', back_populates='carts')
+
+    def format(self):
+        return {
+            'id': self.id,
+            'products': jsonify(self.products)
+        }
